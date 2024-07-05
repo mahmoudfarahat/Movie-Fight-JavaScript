25c46dc692f66bb55ae9c377a89303edf645634d
Mahmoud Farahat <mahmoud.ped@gmail.com>
2024-07-05 12:00:43 +0300

    1.2

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
 const response = await axios.get('http://www.omdbapi.com/',{
     params:{
         apikey: 'db72eef7',
-        s:'avengers'
+        s: searchTerm
     }
 });
 
 console.log(response.data)
 }
+
+const debounce = (func,delay = 1000) => {
+    let timeoutId;
+    return (...args) =>{
+            if(timeoutId){
+                clearTimeout(timeoutId)
+            }
+            timeoutId  = setTimeout(()=>{
+                func.apply(null,args)
+            },1000)
+    }
+}
+
+const input = document.querySelector('input');
+
+ 
+const onInput = event => { 
+        fetchData(event.target.value);
+};
+
+
+input.addEventListener('input',debounce(onInput,500))
